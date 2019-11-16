24f7b9ae4702a96b98493bcf5ad499d62dc708c6
Anup Deshpande <deshpande.anup24@gmail.com>
2019-11-15 19:52:26 -0500

    add addPost route

--- routes/feedRoutes.js
+++ routes/feedRoutes.js
@@ -13,13 +13,38 @@ router.get('/',async function(req,res){
     axios.get('https://college-event-portal-api.herokuapp.com/api/posts', 
     {headers : header})
     .then(response => {
-        console.log(response);
+
+        res.render('feeds',{feeds: response.data});
+
+    }).catch(err => {
+        console.log(err);
+    })
+    
+});
+
+
+// Add new post route
+router.post('/addPost', async function(req,res){
+
+    // Create header with token
+    const header = {
+        "Content-Type":"application/json",
+        "Authorization": req.cookies.token
+    }
+
+    axios.post('https://college-event-portal-api.herokuapp.com/api/posts/addPost', 
+    {
+	    title: req.body.post
+    },
+    {headers : header}
+    ).then(response => {
+       // Post added successfully. Refresh feeds page
+       res.redirect('/feeds/');
+
     }).catch(err => {
         console.log(err);
     })
 
-    res.render('feeds');
-    
 });
 
 module.exports = router;
